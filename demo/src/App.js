ee7669e1d9968b3e6ff2663ed3d03f17ad8b23f7
Matthew Morgan <matthewmorganjoh@gmail.com>
2023-07-02 16:52:12 -0700

    Build is run

---
+++ demo/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> React demo for deployment</h1>
+      <p>new words</p>
+    </div>
+  );
+}
+
+export default App;
